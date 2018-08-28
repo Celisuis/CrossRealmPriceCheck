d96d09527aac9fdf07d0c46bdebc2aad553eb19b
Celisuis <rhian.macham@outlook.com>
2018-08-28 15:56:39 +0100

    Fixed Closing Bug.

--- CrossRealmCheck.cs
+++ CrossRealmCheck.cs
@@ -20,6 +20,22 @@ namespace CrossRealmPriceCheck
         public CrossRealmCheck()
         {
             InitializeComponent();
+            FormClosing += new FormClosingEventHandler(CloseForm);
+        }
+
+        private void CloseForm(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                if (MessageBox.Show("Are you sure want to exit?",
+                               "Cross Realm Price Check",
+                                MessageBoxButtons.OKCancel,
+                                MessageBoxIcon.Information) == DialogResult.OK)
+                    Environment.Exit(1);
+                else
+                    e.Cancel = true; // to don't close form is user change his mind
+            }
+
         }
 
         private void Realm_Name_Textbox_TextChanged(object sender, EventArgs e)
